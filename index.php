efb96caae169fed08bed31e0a0817f177c118b56
TavaresDylan <tavares-dylan03@gmail.com>
2019-04-05 15:27:20 +0200

    Exercice 1 Complet

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
-
+//Exercice 1
+//initialisation de la variable à 0
+$papate = 0;
+//Boucle de la variable
+//Tant que i d'atteint pas 10 
+for ($i=0; $i < 10; $i++) { 
+	//Variable s'incrémente de +1 à chaque itération
+	$papate = $i;
+	//On affiche la variable papate à chaque itération
+	echo "<br />".($papate);
+}
+//Exercice 2
+//Initialisation de 2 variables
+$neptune = 0;
+$champoing = 
 
 
 
